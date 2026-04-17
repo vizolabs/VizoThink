d4e37ad32c467fbab5a427fb4f432381cab24669
VizoLabs <vizolabs@syndicate.dev>
2026-04-17 10:38:57 +0530

    feat: add components barrel export

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar'
+export { default as Footer } from './Footer'
+export { default as Hero } from './Hero'
+export { default as FeatureCard } from './FeatureCard'
+export { default as Button } from './Button'
